e1848d763bae932e9360d68ad9a43168f28e9943
MH03ElN <hoseinkarbasi287@gmail.com>
2024-11-19 19:40:19 +0330

    created views and connected to urls
    created tree views for index , about and contact

--- website/views.py
+++ website/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index_view(requests):
+    return render(requests , 'website/index.html')
+
+def about_view(requests):
+    return render(requests , 'website/about.html')
+
+def contact_view(requests):
+    return render(requests , 'website/contact.html')
