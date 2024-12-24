35a10533ee1d9e46ef68e602a56735b0562f8f85
MH03ElN <hoseinkarbasi287@gmail.com>
2024-12-24 17:14:41 +0330

    added templates
    added templates and upgraded settings'py and views to show that

--- website/views.py
+++ website/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_view(requests):
-    pass
+    return render(requests, 'website/index.html')
 
 
 def about_view(requests):
-    pass
+    return render(requests, 'website/about.html')
 
 
 def contact_view(requests):
-    pass
+    return render(requests, 'website/contact.html')
