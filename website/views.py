fa550ae3a3fe3ae4d054303ee637fd3f031eafc7
MH03ElN <hoseinkarbasi287@gmail.com>
2024-12-24 16:52:19 +0330

    created views
    created tree views ( index , about , contact )

--- website/views.py
+++ website/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index_view(requests):
+    pass
+
+
+def about_view(requests):
+    pass
+
+
+def contact_view(requests):
+    pass
