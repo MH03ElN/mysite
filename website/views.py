351a2ba06694dea80d84c8554ea2dd002be8fda7
MH03ElN <hoseinkarbasi287@gmail.com>
2025-02-13 09:50:03 +0330

    created views
    created three views for index, about and contact and added to urls

--- website/views.py
+++ website/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index_views(request):
+    pass
+
+
+def about_views(request):
+    pass
+
+
+def contact_views(request):
+    pass
