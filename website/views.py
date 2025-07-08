c06d1b6aa2a8e3df02febecb793a47d32a924ce0
MH03ElN <hoseinkarbasi287@gmail.com>
2025-07-09 02:32:17 +0330

    created website app
    created first app named website with tree views for index, about and contact

---
+++ website/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index_veiw(request):
+    pass
+
+
+def about_veiw(reauest):
+    pass
+
+
+def contact_veiw(reauest):
+    pass
