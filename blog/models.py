528ac7f1f4a3839826e38f64649022c643bb24b3
MH03ElN <hoseinkarbasi287@gmail.com>
2025-07-14 17:34:58 +0330

    Created Post model
    Created Post model with migrations and customized admin interface

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    status = models.BooleanField(default=False)
+    counted_views = models.IntegerField(default=0)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    # author
+    # image
+    # category
+    # tag
+
+    class Meta:
+        ordering = ["created_date"]
