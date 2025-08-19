e11e3dbe105d98c78b4c88f7e0055ef1f480d7e0
MH03ElN <hoseinkarbasi287@gmail.com>
2025-08-19 23:07:18 +0330

    Added category feature with filtering support
    - Implemented template tag to display categories with post counts
    - Enabled filtering posts by selected category

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,9 +1,19 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, Category
 
 register = template.Library()
 
-@register.inclusion_tag('blog/blog-latest-posts.html')
+
+@register.inclusion_tag("blog/blog-latest-posts.html")
 def latestposts():
     posts = Post.objects.filter(status=1)[:3]
-    return {'posts':posts}
+    return {"posts": posts}
+
+
+@register.inclusion_tag("blog/blog-post-categories.html")
+def categories():
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = Post.objects.filter(status=1, category=name).count()
+    return {"categories": cat_dict}
--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,10 @@ from blog.models import Post
 
 
 # Create your views here.
-def blog_view(request):
+def blog_view(request, cat_name=None):
     posts = Post.objects.filter(status=1)
+    if cat_name:
+        posts = posts.filter(category__name=cat_name)
     context = {"posts": posts}
     return render(request, "blog/blog-home.html", context)
 
@@ -13,3 +15,9 @@ def blog_single(request, pid):
     post = Post.objects.get(id=pid)
     context = {"post": post}
     return render(request, "blog/blog-single.html", context)
+
+
+# def blog_category(request, cat_name):
+#     posts = Post.objects.filter(category__name=cat_name)
+#     context = {"posts":posts}
+#     return render(request, "blog/blog-home.html", context)
