8791e4192a79c45eefaca925d54a60d702a67e7a
MH03ElN <hoseinkarbasi287@gmail.com>
2025-08-24 15:13:20 +0330

    Added pagination for blog posts
    Implemented pagination in blog view and template.
    Posts are now displayed in multiple pages with navigation controls
    (previous, next, and numbered page links).

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from blog.models import Post
 from django.db.models import Q
-
+from django.core.paginator import Paginator
 
 # Create your views here.
 def blog_view(request, cat_name=None, author_username=None):
@@ -10,6 +10,9 @@ def blog_view(request, cat_name=None, author_username=None):
         posts = posts.filter(category__name=cat_name)
     if author_username:
         posts = posts.filter(author__username=author_username)
+    posts = Paginator(posts, 3)
+    page_number = request.GET.get("page")
+    posts = posts.get_page(page_number)
     context = {"posts": posts}
     return render(request, "blog/blog-home.html", context)
 
