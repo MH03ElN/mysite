a277d6ee8033590ee7790e2106edd6624841375b
MH03ElN <hoseinkarbasi287@gmail.com>
2026-02-22 03:41:03 +0330

    Created views and configured URLs
    - Created index, about, and contact views
    - Added URL patterns for each view
    - Connected views to templates

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,10 @@
-from django.contrib import admin
+from django.urls import path
+from website.views import *
+
+name = "website"
 
 urlpatterns = [
-   
+    path("", index_view, name="index"),
+    path("about/", index_view, name="index"),
+    path("contact/", index_view, name="index"),
 ]
